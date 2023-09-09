191937d714e9b706146e06e97abeaae96da6e6a6
JaredLBailey <jared.l.bailey@gmail.com>
2023-09-09 17:31:24 -0400

    Update app.py

--- app.py
+++ app.py
@@ -73,6 +73,10 @@ with col2:
     st.image(image_3)
     st.header("Burnout Level")
 
+st.write("")
+st.write("")
+st.write("")
+
 col_01, col_02, col_03 = st.columns([2, 1, 2])
 with col_02:
     daily_checking_button = st.button("Daily Checkin", type="primary")
