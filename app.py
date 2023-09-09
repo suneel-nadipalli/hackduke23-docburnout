ab45cf7981077e85bc5e454dae2bc86dd1f87b85
JaredLBailey <jared.l.bailey@gmail.com>
2023-09-09 14:58:29 -0400

    Update app.py

--- app.py
+++ app.py
@@ -2,17 +2,30 @@ import streamlit as st
 
 st.title("APP NAME HERE")
 
-st.write("My Progress")
+st.header("My Progress")
 st.write("GRAPH HERE")
 
-st.write("DAILY CHECKIN BUTTON HERE")
+col1, col2 = st.columns(2)
 
-st.write("FOOTER HERE")
-st.write("FOOTER NAVIGATION HERE")
+with col1:
+   st.header("Stress Level")
+   st.write("MEDIUM STRESS CIRCLE CHART HERE")
 
+with col2:
+   st.header("Burnout Level")
+   st.write("MEDIUM BURNOUT CIRCLE CHART HERE")
 
+st.button("Daily Checkin")
 
+st.write("")
+st.write("")
 
-
-
-
+footer = st.container()
+with footer:
+    col1, col2, col3= st.columns((2,1,1))
+    with col1:
+        st.write("Home")
+    with col2:
+        st.write("Actions")
+    with col3:
+        st.write("Daily Checkin")
