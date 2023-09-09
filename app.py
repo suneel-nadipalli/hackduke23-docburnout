4b8bf2bb2bcd498d7065a63140289e560c2ee83a
JaredLBailey <jared.l.bailey@gmail.com>
2023-09-09 16:13:21 -0400

    Update app.py

--- app.py
+++ app.py
@@ -9,7 +9,8 @@ st.set_page_config(
 st.title("APP NAME HERE")
 
 st.header("My Progress")
-st.write("GRAPH HERE")
+image_1 = Image.open('img/Orange Circle.png')
+st.image(image_1)
 
 col1, col2 = st.columns(2)
 newsize = (200, 200)
