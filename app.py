4cb25568b6a89ec9ea9efed65a2032f67f1f34ed
JaredLBailey <jared.l.bailey@gmail.com>
2023-09-09 14:40:38 -0400

    Add files via upload

---
+++ app.py
@@ -0,0 +1,18 @@
+import streamlit as st
+
+st.title("APP NAME HERE")
+
+st.write("My Progress")
+st.write("GRAPH HERE")
+
+st.write("DAILY CHECKIN BUTTON HERE")
+
+st.write("FOOTER HERE")
+st.write("FOOTER NAVIGATION HERE")
+
+
+
+
+
+
+
