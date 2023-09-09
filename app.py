a3e65e4ed18d9acda5f45e1367c0c83a7ab692eb
suneel-nadipalli <140884446+suneel-nadipalli@users.noreply.github.com>
2023-09-10 04:32:48 +0530

    Update app.py

--- app.py
+++ app.py
@@ -22,7 +22,7 @@ st.title("Spa MD")
 
 st.header("My Progress")
 
-@st.cache_data(ttl=600)
+@st.cache_data(ttl=5)
 def load_data(sheets_url):
     csv_url = sheets_url.replace("/edit#gid=", "/export?format=csv&gid=")
     return pd.read_csv(csv_url)
