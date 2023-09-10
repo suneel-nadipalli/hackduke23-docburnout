bb39e494aeb5458392dab4067187c88c21f2a2a4
JaredLBailey <jared.l.bailey@gmail.com>
2023-09-09 22:16:40 -0400

    Update Fatigue_Check.py

--- pages/Fatigue_Check.py
+++ pages/Fatigue_Check.py
@@ -8,15 +8,15 @@ img_file_buffer = st.camera_input("")
 
 if img_file_buffer is not None:
    # To read image file buffer as a PIL Image:
-    img = Image.open(img_file_buffer)
+    # img = Image.open(img_file_buffer)
 
     # To convert PIL Image to numpy array:
-    img_array = np.array(img)
+    # img_array = np.array(img)
 
     # Check the type of img_array:
     # Should output: <class 'numpy.ndarray'>
-    st.write(type(img_array))
+    # st.write(type(img_array))
 
     # Check the shape of img_array:
     # Should output shape: (height, width, channels)
-    st.write(img_array.shape)
+    # st.write(img_array.shape)
