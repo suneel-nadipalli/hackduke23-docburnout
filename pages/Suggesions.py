e2005bfe6f03b7b0c8374749d870b7ef02b2f05b
JaredLBailey <jared.l.bailey@gmail.com>
2023-09-09 20:57:49 -0400

    Update Suggesions.py

--- pages/Suggesions.py
+++ pages/Suggesions.py
@@ -4,9 +4,8 @@ st.title("Suggestions")
 
 col_0_1, col_0_2 = st.columns(2)
 
-form1 = st.form(key='my_form')
 with col_0_1:
-    form1.header("Sleep")
-    form1.write("Nap 20 minutes to reduce tiredness")
+    st.header("Sleep")
+    st.write("Nap 20 minutes to reduce tiredness")
 with col_0_2:
-    submit = form1.form_submit_button('Complete')
+    Complete1 = st.form_submit_button('Complete')
