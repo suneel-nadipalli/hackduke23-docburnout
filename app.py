741f0ad7ef0ac34595ecbb65473230a74b0d3ad8
JaredLBailey <jared.l.bailey@gmail.com>
2023-09-09 21:49:44 -0400

    Update app.py

--- app.py
+++ app.py
@@ -37,11 +37,11 @@ svm_clf = joblib.load('burnout.pkl')
 # st.set_page_config(initial_sidebar_state="collapsed")
 
 st.set_page_config(
-    page_title="Stress Tracker MD"
+    page_title="Health Tracker"
     # ,page_icon="CHECK"
     )
 
-st.title("Stress Tracker MD")
+# st.title("Health Tracker MD")
 
 st.header("My Progress")
 
