64794ec0477ab4694b1bb0e49f25cb7993ae3efa
JaredLBailey <jared.l.bailey@gmail.com>
2023-09-09 21:16:46 -0400

    Update Image_Capture.py

--- pages/Image_Capture.py
+++ pages/Image_Capture.py
@@ -1,6 +1,10 @@
 import streamlit as st
 
-picture = st.camera_input("Take a picture")
+img_file_buffer = st.camera_input("Take a picture")
 
-if picture:
-    st.image(picture)
+if img_file_buffer is not None:
+    # To read image file buffer as bytes:
+    bytes_data = img_file_buffer.getvalue()
+    # Check the type of bytes_data:
+    # Should output: <class 'bytes'>
+    st.write(type(bytes_data))
