036e5efba65e72cc0f8c9a7bc2d99d2188386095
JaredLBailey <jared.l.bailey@gmail.com>
2023-09-09 20:51:08 -0400

    Update Suggesions.py

--- pages/Suggesions.py
+++ pages/Suggesions.py
@@ -2,7 +2,11 @@ import streamlit as st
 
 st.title("Suggestions")
 
+col_0_1, col_0_2 = st.coluns(2)
+
 form1 = st.form(key='my_form')
-form1.header("Sleep")
-form1.write("Nap 20 minutes to reduce tiredness")
-submit = form1.form_submit_button('Complete')
+with col_0_1:
+    form1.header("Sleep")
+    form1.write("Nap 20 minutes to reduce tiredness")
+with col_0_2:
+    submit = form1.form_submit_button('Complete')
