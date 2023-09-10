9879e3129300b03cd030f313ec133bec36ff6de6
JaredLBailey <jared.l.bailey@gmail.com>
2023-09-09 22:33:55 -0400

    Update Daily_Checkin.py

--- pages/Daily_Checkin.py
+++ pages/Daily_Checkin.py
@@ -82,6 +82,8 @@ question_15 = form.slider("15) I find that I do not have time to plan as much as
 
 submit = form.form_submit_button('Submit')
 
+st.write("You selected:", question_1+question_2+question_3+question_4+question_5+question_6+question_7+question_8+question_9+question_10+question_11+question_12+question_13+question_14+question_15)
+
 
 
 
