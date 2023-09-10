51159ce6f38801ceec62db4d6594db70fb3e5c2f
JaredLBailey <jared.l.bailey@gmail.com>
2023-09-09 20:21:51 -0400

    Update app.py

--- app.py
+++ app.py
@@ -88,7 +88,7 @@ st.write("")
 st.write("")
 st.write("")
 
-col_01, col_02, col_03 = st.columns([2, 1, 3])
+col_01, col_02, col_03 = st.columns([2, 2, 3])
 with col_02:
     daily_checking_button = st.button("Daily Checkin", type="primary")
     if daily_checking_button:
