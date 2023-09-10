c47eac7bb2065b22766b04842c9ab67a46f7b6cc
JaredLBailey <jared.l.bailey@gmail.com>
2023-09-09 23:06:48 -0400

    Update Fatigue_Check.py

--- pages/Fatigue_Check.py
+++ pages/Fatigue_Check.py
@@ -6,11 +6,11 @@ st.set_page_config(initial_sidebar_state="collapsed", layout="centered")
 
 img_file_buffer = st.camera_input("")
 
-   if img_file_buffer is not None:
+if img_file_buffer is not None:
 
-      st.write("Your recent image analysis results indicate a low level of fatigue, suggesting you are likely well-rested and alert.")
-      st.write("Your recent image analysis results indicate a moderate level of fatigue, suggesting you may benefit from some rest or a break to maintain optimal alertness.")
-      st.write("Your recent image analysis results indicate a high level of fatigue, suggesting you may be experiencing significant tiredness or exhaustion. ")
+   st.write("Your recent image analysis results indicate a low level of fatigue, suggesting you are likely well-rested and alert.")
+   st.write("Your recent image analysis results indicate a moderate level of fatigue, suggesting you may benefit from some rest or a break to maintain optimal alertness.")
+   st.write("Your recent image analysis results indicate a high level of fatigue, suggesting you may be experiencing significant tiredness or exhaustion. ")
    # To read image file buffer as a PIL Image:
     # img = Image.open(img_file_buffer)
 
