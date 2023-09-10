21049d61af97d64f0c120b05eaf31df67837b82c
JaredLBailey <jared.l.bailey@gmail.com>
2023-09-09 23:09:13 -0400

    Update Fatigue_Check.py

--- pages/Fatigue_Check.py
+++ pages/Fatigue_Check.py
@@ -10,10 +10,13 @@ img_file_buffer = st.camera_input("")
 if img_file_buffer is not None:
    random_number = random.randint(1, 3)
    if random_number == 1:
+      st.header("Low Fatigue")
       st.write("Your recent image analysis results indicate a low level of fatigue, suggesting you are likely well-rested and alert.")
    elif random_number == 2:
+      st.header("Moderate Fatigue")
       st.write("Your recent image analysis results indicate a moderate level of fatigue, suggesting you may benefit from some rest or a break to maintain optimal alertness.")
    else:
+      st.header("High Fatigue")
       st.write("Your recent image analysis results indicate a high level of fatigue, suggesting you may be experiencing significant tiredness or exhaustion. ")
    # To read image file buffer as a PIL Image:
     # img = Image.open(img_file_buffer)
