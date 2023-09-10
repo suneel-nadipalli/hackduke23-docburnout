3d22a84f6cdf5494f95497385a3616f6a5e38851
JaredLBailey <jared.l.bailey@gmail.com>
2023-09-09 21:19:24 -0400

    Update Image_Capture.py

--- pages/Image_Capture.py
+++ pages/Image_Capture.py
@@ -3,8 +3,16 @@ import streamlit as st
 img_file_buffer = st.camera_input("Take a picture")
 
 if img_file_buffer is not None:
-    # To read image file buffer as bytes:
-    bytes_data = img_file_buffer.getvalue()
-    # Check the type of bytes_data:
-    # Should output: <class 'bytes'>
-    # st.write(type(bytes_data))
+   # To read image file buffer as a PIL Image:
+    img = Image.open(img_file_buffer)
+
+    # To convert PIL Image to numpy array:
+    img_array = np.array(img)
+
+    # Check the type of img_array:
+    # Should output: <class 'numpy.ndarray'>
+    st.write(type(img_array))
+
+    # Check the shape of img_array:
+    # Should output shape: (height, width, channels)
+    st.write(img_array.shape)
