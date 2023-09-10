a51fdf9d0e359116d0c8e90dc97a6997ba33e052
JaredLBailey <jared.l.bailey@gmail.com>
2023-09-09 20:33:56 -0400

    Update Daily_Checkin.py

--- pages/Daily_Checkin.py
+++ pages/Daily_Checkin.py
@@ -22,63 +22,63 @@ with col7:
 
 form = st.form(key='my_form')
 
-question_01 = form.slider("Question 1: I feel run down and drained of physical or emotional energy", 1, 7, 4)
+question_01 = form.slider("1: I feel run down and drained of physical or emotional energy", 1, 7, 4)
 
 form.write("")
 form.write("")
-question_02 = form.slider("Question 2: I have negative thoughts about my job", 1, 7, 4)
+question_02 = form.slider("2: I have negative thoughts about my job", 1, 7, 4)
 
 form.write("")
 form.write("")
-question_03 = form.slider("Question 3: I am harder and less sympathetic with people than perhaps they deserve", 1, 7, 4)
+question_03 = form.slider("3: I am harder and less sympathetic with people than perhaps they deserve", 1, 7, 4)
 
 form.write("")
 form.write("")
-question_04 = form.slider("Question 4: I am easily irritated by small problems, or by my co-workers and team", 1, 7, 4)
+question_04 = form.slider("4: I am easily irritated by small problems, or by my co-workers and team", 1, 7, 4)
 
 form.write("")
 form.write("")
-question_05 = form.slider("Question 5: I feel misunderstood or unappreciated by my co-workers", 1, 7, 4)
+question_05 = form.slider("5: I feel misunderstood or unappreciated by my co-workers", 1, 7, 4)
 
 form.write("")
 form.write("")
-question_06 = form.slider("Question 6: I feel that I have no one to talk to", 1, 7, 4)
+question_06 = form.slider("6: I feel that I have no one to talk to", 1, 7, 4)
 
 form.write("")
 form.write("")
-question_07 = form.slider("Question 7: I feel that I am achieving less than I should", 1, 7, 4)
+question_07 = form.slider("7: I feel that I am achieving less than I should", 1, 7, 4)
 
 form.write("")
 form.write("")
-question_08 = form.slider("Question 8: I feel under an unpleasant level of pressure to succeed", 1, 7, 4)
+question_08 = form.slider("8: I feel under an unpleasant level of pressure to succeed", 1, 7, 4)
 
 form.write("")
 form.write("")
-question_09 = form.slider("Question 9: I feel that I am not getting what I want out of my job", 1, 7, 4)
+question_09 = form.slider("9: I feel that I am not getting what I want out of my job", 1, 7, 4)
 
 form.write("")
 form.write("")
-question_10 = form.slider("Question 10: I feel that I am in the wrong organization or the wrong profession", 1, 7, 4)
+question_10 = form.slider("10: I feel that I am in the wrong organization or the wrong profession", 1, 7, 4)
 
 form.write("")
 form.write("")
-question_11 = form.slider("Question 11: I am frustrated with parts of my job", 1, 7, 4)
+question_11 = form.slider("11: I am frustrated with parts of my job", 1, 7, 4)
 
 form.write("")
 form.write("")
-question_12 = form.slider("Question 12: I feel that organizational politics or bureaucracy frustrate my ability to do a good job", 1, 7, 4)
+question_12 = form.slider("12: I feel that organizational politics or bureaucracy frustrate my ability to do a good job", 1, 7, 4)
 
 form.write("")
 form.write("")
-question_13 = form.slider("Question 13: I feel that there is more work to do than I practically have the ability to do", 1, 7, 4)
+question_13 = form.slider("13: I feel that there is more work to do than I practically have the ability to do", 1, 7, 4)
 
 form.write("")
 form.write("")
-question_14 = form.slider("Question 14: I feel that I do not have time to do many of the things that are important to doing a good quality job", 1, 7, 4)
+question_14 = form.slider("14: I feel that I do not have time to do many of the things that are important to doing a good quality job", 1, 7, 4)
 
 form.write("")
 form.write("")
-question_15 = form.slider("Question 15: I find that I do not have time to plan as much as I would like to", 1, 7, 4)
+question_15 = form.slider("15: I find that I do not have time to plan as much as I would like to", 1, 7, 4)
 
 submit = form.form_submit_button('Submit')
 
